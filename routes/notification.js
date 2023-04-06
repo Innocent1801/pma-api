89feae6a423b70707ffa8a63402d6d787863493c
innocent98 <tosinadebayo55@gmail.com>
2023-04-06 09:17:04 +0100

    updated

--- routes/notification.js
+++ routes/notification.js
@@ -5,10 +5,10 @@ const Notification = require("../models/Notification");
 router.post("/", async (req, res) => {
   try {
     const newNotification = new Notification({
-        notId: req.body.notId,
-        notTitle: req.body.notTitle,
-        notification: req.body.notification,
-        url: req.body.url,
+      notId: req.body.notId,
+      notTitle: req.body.notTitle,
+      notification: req.body.notification,
+      // url: req.body.url,
     });
     await newNotification.save();
     res.status(200).json(newNotification);
@@ -31,6 +31,7 @@ router.get("/:id", async (req, res) => {
 router.get("/single-not/:id", async (req, res) => {
   try {
     const notifications = await Notification.findById(req.params.id);
+    await notifications.updateOne({ isRead: true });
     res.status(200).json(notifications);
   } catch (err) {
     res.status(500).json("Connection error!");
