c1009a59846c708c3703e4068da5cbf9589f7e5b
innocent98 <tosinadebayo55@gmail.com>
2023-05-16 04:46:01 +0000

    updated

--- models/Blog.js
+++ models/Blog.js
@@ -3,10 +3,15 @@ const mongoose = require("mongoose");
 const BlogSchema = new mongoose.Schema(
   {
     title: { type: String },
-    text: { type: String },
     photo: { type: String },
     likes: { type: Array },
     cat: { type: String },
+    paragraphs: [
+      {
+        text: { type: String },
+        image: { type: String },
+      },
+    ],
   },
   { timestamps: true }
 );
